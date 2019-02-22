260a7c1d8bf8d3ed65c33037024fde4cd01a147f
Nicolas Escorcia <nicodevcode@gmail.com>
2019-02-21 20:33:35 -0500

    second commit

--- animacion.js
+++ animacion.js
@@ -1,138 +1,89 @@
-
-
 class Persona {
-
-    constructor(nombre, edad, estado = false){
-
+    constructor(nombre, edad){
         this.nombre = nombre;
         this.edad = edad;
-        this.estado = estado;
-
     }
-
 }
-
 let arrayPersonas = [];
-function add(){
+const add = () => {
+    let inputNombre = document.querySelector('#nombre').value;
+    let inputEdad = document.querySelector('#edad').value;
 
-    var inputNombre = document.querySelector('#nombre').value;
-    var inputEdad = document.querySelector('#edad').value;
+    if (parseInt(indexEvent) >= 0){
+        UpDateAlert(inputNombre,inputEdad,indexEvent);
 
-
-
-    if (inputNombre === '' || inputEdad === ''){
-        alert('por favor llene ambos campos')
-    } else {
-
-        let newPersona =new Persona(inputNombre,inputEdad);
-
+    }else {
+        let newPersona =new  Persona(inputNombre,inputEdad);
         arrayPersonas.push(newPersona);
-
-        console.log(newPersona);
-        console.log(arrayPersonas);
-
-
-        GuardarDb();
-        resetInput();
-        renderAlertUi();
     }
 
+    RenderAlertAdd();
+    GuardarBD();
+    cleatInputs();
+    indexEvent = -1
 
-}
 
-const resetInput = () => {
 
+};
+const cleatInputs = () =>{
     this.nombre.value = '';
     this.edad.value = '';
-
 };
-
-const GuardarDb  = () => {
-
-    localStorage.setItem('persona', JSON.stringify(arrayPersonas))
-
+const GuardarBD = () => {
+    localStorage.setItem('PersonasLocalDB', JSON.stringify(arrayPersonas))
 };
-
-
-const PintarDB = () => {
-
-    arrayPersonas =  JSON.parse(localStorage.getItem('persona'))
-    console.log(arrayPersonas)
-
+const RenderAlertAdd = () => {
+    let RenderUi = document.querySelector('#viewPersonas');
+    RenderUi.innerHTML = '';
+    arrayPersonas.forEach((element, index)=>{
+        RenderUi.innerHTML +=  `<div class="alert alert-success" role="alert" data-index="${index}"><i class="material-icons float-left">accessibility_new</i><b>${element.nombre}</b> - ${element.edad}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete_forever</i></span></div>`
+    })
+};
+const PrintDB = () =>{
+    arrayPersonas = JSON.parse(localStorage.getItem('PersonasLocalDB'));
     if (arrayPersonas === null){
-        arrayPersonas = [];
+        arrayPersonas = []
     } else {
-
-        renderAlertUi();
-
+        RenderAlertAdd()
     }
-
 };
-
-var lista_personasUI = document.querySelector('#lista_personas');
-const renderAlertUi = () => {
-    lista_personasUI.innerHTML = '';
-    arrayPersonas.forEach(element =>{
-
-        if (element.estado){
-            lista_personasUI.innerHTML+= `<div class="alert alert-success" role="alert"><i class="material-icons float-left">accessibility_new</i><b>${element.nombre}</b> - ${element.edad}  - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete_forever</i></span></div>`
-        } else {
-            lista_personasUI.innerHTML+= `<div class="alert alert-primary" role="alert"><i class="material-icons float-left">accessibility_new</i><b>${element.nombre}</b> - ${element.edad}  - ${element.estado}<span class="float-right"><i class="material-icons">done</i><i class="material-icons">delete_forever</i></span></div>`
-        }
-
-    } )
-};
-
-
-
-lista_personasUI.addEventListener('click', (e) => {
+document.addEventListener('DOMContentLoaded', PrintDB);
+let indexEvent;
+document.addEventListener('click', (e)=>{
     e.preventDefault();
-
-    if (e.target.innerHTML === 'done' || e.target.innerHTML === 'delete_forever' ){
-        let texto = e.path[2].childNodes[1].innerHTML;
-        if (e.target.innerHTML === 'delete_forever'){
-            EliminarDb(texto);
+    if (e.target.innerHTML === 'done' || e.target.innerHTML === 'delete_forever') {
+        indexEvent = e.path[2].getAttribute("data-index");
+        if (e.target.innerHTML === 'done') {
+            EditAlert(indexEvent)
         }
-        if (e.target.innerHTML === 'done'){
-            EditarDb(texto);
 
+        if (e.target.innerHTML === 'delete_forever') {
+            DeleteAlert(indexEvent)
         }
     }
 });
 
-
-const EliminarDb = (nombre) =>{
-
-    let indexArry;
-    arrayPersonas.forEach((element, index) =>{
-
-        
-        if (element.nombre === nombre) {
-            indexArry = index;
-            console.log('se elimino la posicion '+ ' ' + indexArry)
-        }
-
-    });
-
-    arrayPersonas.splice(indexArry,1);
-    GuardarDb();
-    renderAlertUi();
-
-    console.log(nombre)
+const EditAlert = () => {
+    this.nombre.value = arrayPersonas[indexEvent].nombre;
+    this.edad.value = arrayPersonas[indexEvent].edad;
 
 };
 
-const EditarDb = (nombre) => {
+const UpDateAlert = (nombre,edad) => {
 
-    let indexArry = arrayPersonas.findIndex((element) => {
-      return  element.nombre === nombre
-    });
+    arrayPersonas[parseInt(indexEvent)]['nombre'] = nombre;
+    arrayPersonas[parseInt(indexEvent)]['edad'] = edad;
 
-    console.log(indexArry);
-    console.log(arrayPersonas[indexArry]);
-    arrayPersonas[indexArry].estado = true;
-    GuardarDb();
-    renderAlertUi();
+    RenderAlertAdd();
+    GuardarBD();
+    cleatInputs();
 };
 
-document.addEventListener('DOMContentLoaded', PintarDB);
+const DeleteAlert = () =>{
+    arrayPersonas.splice([parseInt(indexEvent)],1);
+    RenderAlertAdd();
+    GuardarBD();
+    cleatInputs();
+    indexEvent = -1
+};
+
